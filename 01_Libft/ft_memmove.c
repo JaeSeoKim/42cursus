c996bd3789c4fc1c6acd5c4c3b90d258778e9157
JaeSeoKim <devjaeseo@gmail.com>
2020-09-29 15:22:24 +0900

    01_Libft - fix ft_memmove

--- 01_Libft/ft_memmove.c
+++ 01_Libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 21:49:07 by jaeskim           #+#    #+#             */
-/*   Updated: 2020/09/25 06:26:18 by jaeskim          ###   ########.fr       */
+/*   Updated: 2020/09/29 15:11:41 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*dp;
 	const unsigned char	*sp;
 
+	if (!dest || !src)
+		return (0);
 	if (dest > src)
 	{
 		dp = dest + n;
