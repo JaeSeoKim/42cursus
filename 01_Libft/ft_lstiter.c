db973d32ba88877b10ce93b2eabd03b460e36b47
JaeSeoKim <devjaeseo@gmail.com>
2020-09-30 22:10:08 +0900

    [Libft] remove unicode typo!

--- 01_Libft/ft_lstiter.c
+++ 01_Libft/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 00:14:29 by jaeskim           #+#    #+#             */
-/*   Updated: 2020/09/30 19:45:40 by jaeskim          ###   ########.fr       */
+/*   Updated: 2020/09/30 22:10:02 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 /*
 ** ft_lstiter - Iterates the list 'lst' and applies the function
-** ’f’ to the content of each element.
+** 'f' to the content of each element.
 */
 
 void	ft_lstiter(t_list *lst, void (*f)(void *))
