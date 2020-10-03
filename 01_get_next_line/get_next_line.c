dab5f3181f64706b32dae37258c151f3239a0774
JaeSeoKim <devjaeseo@gmail.com>
2020-10-04 00:01:41 +0900

    [get_next_line] prototype done!

---
+++ 01_get_next_line/get_next_line.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 17:48:48 by jaeskim           #+#    #+#             */
+/*   Updated: 2020/10/04 00:01:27 by jaeskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static int	storejoin(char **store, char *buf, ssize_t read_size)
+{
+	char	*tmp;
+
+	buf[read_size] = '\0';
+	if (0 != *store)
+	{
+		tmp = ft_strjoin(*store, buf);
+		free(*store);
+	}
+	else
+		tmp = ft_strndup(buf, read_size);
+	*store = tmp;
+	if (ft_strchr(*store, '\n'))
+		return (1);
+	return (0);
+}
+
+static int	ft_check_line(char **store, char **line)
+{
+	char	*ptr;
+	char	*tmp;
+	if ((ptr = ft_strchr(*store, '\n')))
+	{
+		*line = ft_strndup(*store, ptr - *store);
+		tmp = ft_strndup(ptr + 1, ft_strlen(ptr + 1));
+		free(*store);
+		*store = 0;
+		*store = tmp;
+		return (1);
+	}
+	else
+	{
+		*line = *store;
+		*store = 0;
+		return (0);
+	}
+};
+
+int			get_next_line(int fd, char **line)
+{
+	static char	*store[OPEN_MAX];
+	ssize_t		read_size;
+	char		*buf;
+
+	if (fd <= OPEN_MAX && !line && BUFFER_SIZE <= 0)
+		return (-1);
+	if (!(buf = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (-1);
+	while ((read_size = read(fd, buf, BUFFER_SIZE)) && read_size > 0)
+	{
+		if (storejoin(&store[fd], buf, read_size))
+			break;
+	}
+	free(buf);
+	if (read_size < 0)
+		return (-1);
+	return (ft_check_line(&store[fd], line));
+}
---
+++ 01_get_next_line/get_next_line.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/03 17:49:05 by jaeskim           #+#    #+#             */
+/*   Updated: 2020/10/03 21:59:27 by jaeskim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+/*
+** windows, linux doesn't have OPEN_MAX Constants
+** OPEN_MAX value from Mac Os
+*/
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX	10240
+# endif
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *s);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strndup(const char *s, size_t n);
+char	*ft_strchr(const char *s, int c);
+
+#endif
