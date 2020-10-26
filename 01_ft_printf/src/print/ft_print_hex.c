5b617b54e52a1e6ecf81ba4424feab9c52681869
jaeskim <jaeskim@student.42seoul.kr>
2020-10-26 22:56:29 +0900

    [ft_printf] fix print `"%3x", 0`

--- 01_ft_printf/src/print/ft_print_hex.c
+++ 01_ft_printf/src/print/ft_print_hex.c
@@ -6,27 +6,28 @@
 /*   By: jaeskim <jaeskim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/18 17:03:18 by jaeskim           #+#    #+#             */
-/*   Updated: 2020/10/26 22:28:43 by jaeskim          ###   ########.fr       */
+/*   Updated: 2020/10/26 22:54:59 by jaeskim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
 
-static void	ft_print_flag(t_format *pf)
+static void	ft_print_flag(t_format *pf, unsigned long long int n)
 {
-	if (pf->flag.hash)
+	if (pf->flag.hash && n != 0)
 		ft_putstr_out(pf->out, (**pf->ptr) == 'x' ? "0x" : "0X");
 }
 
 static int	ft_calc_width(
+	unsigned long long int n,
 	int n_len,
 	t_format *pf)
 {
 	int result;
 
 	result = (pf->precision > n_len ? pf->precision : n_len);
-	if (pf->flag.hash)
+	if (pf->flag.hash && n != 0)
 		result += 2;
 	return (result);
 }
@@ -51,30 +52,31 @@ static void	ft_print_precision_with_num(
 }
 
 static void	ft_print_format(
+	unsigned long long int n,
 	int cnt,
 	t_format *pf,
 	char *n_str)
 {
 	if (pf->flag.zero && !pf->flag.dash && !pf->visit_precision)
 	{
-		ft_print_flag(pf);
+		ft_print_flag(pf, n);
 		ft_putchar_n_out(pf->out, cnt - \
-			ft_calc_width(ft_strlen(n_str), pf), '0');
+			ft_calc_width(n, ft_strlen(n_str), pf), '0');
 		ft_print_precision_with_num(ft_strlen(n_str), pf, n_str);
 	}
 	else if (!pf->flag.dash)
 	{
 		ft_putchar_n_out(pf->out, cnt - \
-			ft_calc_width(ft_strlen(n_str), pf), ' ');
-		ft_print_flag(pf);
+			ft_calc_width(n, ft_strlen(n_str), pf), ' ');
+		ft_print_flag(pf, n);
 		ft_print_precision_with_num(ft_strlen(n_str), pf, n_str);
 	}
 	else
 	{
-		ft_print_flag(pf);
+		ft_print_flag(pf, n);
 		ft_print_precision_with_num(ft_strlen(n_str), pf, n_str);
 		ft_putchar_n_out(pf->out, cnt - \
-			ft_calc_width(ft_strlen(n_str), pf), ' ');
+			ft_calc_width(n, ft_strlen(n_str), pf), ' ');
 	}
 }
 
@@ -89,11 +91,14 @@ int			ft_print_hex(va_list ap, t_format *pf)
 	n_str = ft_convert_base_custom(n,\
 		(**pf->ptr) == 'x' ? "0123456789abcdef" : "0123456789ABCDEF", 16, pf);
 	n_len = ft_strlen(n_str);
-	cnt = pf->width > n_len + 2 ? pf->width : n_len;
+	if (n == 0)
+		cnt = pf->width > n_len ? pf->width : n_len;
+	else
+		cnt = pf->width > n_len + 2 ? pf->width : n_len;
 	cnt = pf->precision > cnt ? pf->precision : cnt;
-	if ((cnt == pf->precision || cnt == n_len) && pf->flag.hash)
+	if ((cnt == pf->precision || cnt == n_len) && pf->flag.hash && n != 0)
 		cnt += 2;
-	ft_print_format(cnt, pf, n_str);
+	ft_print_format(n, cnt, pf, n_str);
 	++(*pf->ptr);
 	ft_frees(2, n_str, pf);
 	return (cnt);
