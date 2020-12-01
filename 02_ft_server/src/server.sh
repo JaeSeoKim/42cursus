a9844e723b5f99b7e8a1c4328bf01e3318059f06
jaeskim <jaeskim@student.42seoul.kr>
2020-12-01 16:28:17 +0900

    [ft_server] finish!

--- 02_ft_server/src/server.sh
+++ 02_ft_server/src/server.sh
@@ -2,6 +2,8 @@
 
 /bin/bash -C /setup_autoindex.sh $1
 
+service mysql start
+service php7.3-fpm start
 service nginx start
 
 if [ $? -eq 0 ]; then
--- 02_ft_server/src/setup_autoindex.sh
+++ 02_ft_server/src/setup_autoindex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # setup index.html
-if [ -f "/var/www/html/index.nginx-debian.html" ]; then
+if [ -e "/var/www/html/index.nginx-debian.html" ]; then
 	mv /var/www/html/index.nginx-debian.html /var/www/html/index.html
 fi
 
 if [ "$1" == "autoindex" ]; then
 	echo "autoindex on!"
-	sed -i "s@autoindex off;@autoindex on;@g" /etc/nginx/sites-available/default
-	sed -i "s@index index.html index.htm;@index index.htm;@g" /etc/nginx/sites-available/default
+	sed -i "s/autoindex off;/autoindex on;/g" /etc/nginx/sites-available/default
+	sed -i "s/index index.html index.htm index.php;/index index.htm index.php;/g" /etc/nginx/sites-available/default
 else
 	echo "autoindex off!"
-	sed -i "s@autoindex on;@autoindex off;@g" /etc/nginx/sites-available/default
-	sed -i "s@index index.htm;@index index.html index.htm;@g" /etc/nginx/sites-available/default
+	sed -i "s/autoindex on;/autoindex off;/g" /etc/nginx/sites-available/default
+	sed -i "s/index index.htm index.php;/index index.html index.htm index.php;/g" /etc/nginx/sites-available/default
 fi
